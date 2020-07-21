7f525e487d24169dfe94b4b6b520a51415eca556
DNAbased <frederic.thieme@uni-bonn.de>
2020-07-21 17:34:07 +0200

    minor changes in data; simplified script

--- get_data.py
+++ get_data.py
@@ -3,7 +3,7 @@ def main():
 
     def convert_data(file_in, file_out):
         input_data = pd.read_excel(file_in)
-        input_data = input_data[['pages', 'language', 'price', 'price_per_page', 'gift']]
+        input_data = input_data[['Pages', 'Language', 'Price', 'Price_per_page', 'Gift']]
         input_data.to_csv(file_out, index=False)
 
 
--- main.py
+++ main.py
@@ -1,50 +1,36 @@
-import pandas as pd
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
-from sklearn.svm import LinearSVC
-from sklearn.metrics import accuracy_score
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
+def main():
+    import pandas as pd
 
-books = pd.read_csv('book_data.csv')
-le = preprocessing.LabelEncoder()
-books['gift'] = le.fit_transform(books['gift'])
-books['language'] = le.fit_transform(books['language'])
+    from sklearn.model_selection import train_test_split
+    from sklearn import preprocessing
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.metrics import accuracy_score
+    from joblib import dump, load # not implemented yet
+    
+    books = pd.read_csv('book_data.csv')
+    le = preprocessing.LabelEncoder()
+    books['Gift'] = le.fit_transform(books['Gift'])
+    books['Language'] = le.fit_transform(books['Language'])
+    
+    X = books[['Price', 'Pages', 'Price_per_page', 'Language']]
+    y = books[['Gift']]
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 321)
+      
+    y_train = y_train.values.ravel()
+    y_test = y_test.values.ravel()
 
-books_x = books[['price', 'pages', 'price_per_page']]
-books_y = books[['gift']]
+    # scaler = preprocessing.StandardScaler().fit(X_train) # lowers accuracy?
+    # X_train_scaled = scaler.transform(X_train)
+    # X_test_scaled = scaler.transform(X_test)
+    
+    KNN = KNeighborsClassifier(n_neighbors=3)
+    
+    KNN.fit(X_train, y_train)
+    
+    prediction = KNN.predict(X_test)
+    
+    print('KNN accuracy : ', accuracy_score(y_test, prediction, normalize = True))
 
-books_x_train, books_x_test, books_y_train, books_y_test = train_test_split(books_x, books_y, test_size = 0.20, random_state = 123)
-
-svc_model = LinearSVC(random_state=0)
-
-books_y_train = books_y_train.values.ravel()
-books_y_test = books_y_test.values.ravel()
-
-pred = svc_model.fit(books_x_train, books_y_train).predict(books_x_test)
-
-print('LinearSVC accuracy : ', accuracy_score(books_y_test, pred, normalize = True))
-
-
-gnb = GaussianNB()
-
-pred = gnb.fit(books_x_train, books_y_train).predict(books_x_test)
-
-print('Gaussian Naive Bayes accuracy : ', accuracy_score(books_y_test, pred, normalize = True))
-
-
-neigh = KNeighborsClassifier(n_neighbors=3)
-
-neigh.fit(books_x_train, books_y_train)
-
-pred = neigh.predict(books_x_test)
-
-print('k-nearest neighbors accuracy score : ', accuracy_score(books_y_test, pred, normalize = True))
-
-
-# so far, my decisions have obviously not solely been based on the data, hence perfect prediction is difficult
-# try with using pages and price instead of price per page, might get better results when using two dimensions
-# analyze effect of the books' language on the decision making process
-
-
-# https://scikit-learn.org/stable/tutorial/machine_learning_map/
+if __name__ == '__main__':
+    main()
